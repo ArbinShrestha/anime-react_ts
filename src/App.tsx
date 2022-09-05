2f3e1a9ab0c628ec4ce1b9f474e1ac14c52e29af
ArbinShrestha <orbeenshr@gmail.com>
2022-09-05 18:00:29 +0545

    fetching api data from jikan

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,47 @@
 import React, { useEffect, useState } from 'react';
-// import NavBar from './components/NavBar'
 import './App.css';
+import AnimeList from './components/AnimeList';
 
 function App() {
 
+  const [search, setSearch]= useState('Naruto')
   const [animeData, setAnimeData] = useState()
 
   const getData = async () => {
-    const res = await fetch(`https://api.jikan.moe/v4/anime?q=naruto&limit=20`)
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
     const resData = await res.json()
     setAnimeData(resData.data)
   }
 
   useEffect(()=>{
     getData()
-  },[])
+  },[search])
 
 
   return (
-    <div className="header">
+    <>
+      <div className="header">
       {/* <NavBar /> */}
       <h1>MyAnimeList</h1>
       <div className="search-box">
-        <input type="text" placeholder='Search your anime'/>
+        <input type="text" placeholder='Search your anime' onChange={(e)=>setSearch(e.target.value)}/>
+      </div>
+      </div>
+
+      <div className="container">
+        <div className="animeInfo">
+          
+        </div>
+          <div className="anime-row">
+            <h2 className="text-heading">Anime</h2>
+            <div className="row">
+              <AnimeList />
+            </div>
+          </div>
       </div>
      
-    </div>
+   
+    </>
   );
 }
 
---
+++ src/components/AnimeList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AnimeList = () => {
+  return (
+    <>
+      <div className="card">
+        <img src="https://cdn.myanimelist.net/images/anime/13/17405.jpg" alt="" className="src" />
+      </div>
+    </>
+  )
+}
+
+export default AnimeList
